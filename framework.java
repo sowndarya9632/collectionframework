964ab42f4e2009202c225f782ceb5d10b7860342
sowndarya9632 <sowndarya9632@gmail.com>
2024-09-30 10:44:04 +0530

    add functionality to check behaviour

--- framework.java
+++ framework.java
@@ -1,54 +1,54 @@
 package com.day1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class PerformanceComparison {
-	private static final int DATA_SIZE = 100000; // Change this for larger datasets
+public class SpecificSetBehavior {
 
 	public static void main(String[] args) {
-		compareSets();
+		  Scanner scanner = new Scanner(System.in);
+	        List<Integer> inputList = new ArrayList<>();
+
+	        System.out.println("Enter integers (type 'done' to finish):");
+	        
+	        while (true) {
+	            if (scanner.hasNextInt()) {
+	                inputList.add(scanner.nextInt());
+	            } else if (scanner.next().equalsIgnoreCase("done")) {
+	                break;
+	            } else {
+	                System.out.println("Please enter a valid integer or 'done' to finish.");
+	            }
+	        }
+	        
+	        Map<String, Set<Integer>> sets = createSets(inputList);
+	        
+	        // Output results
+	        System.out.println("HashSet: " + sets.get("HashSet"));
+	        System.out.println("LinkedHashSet: " + sets.get("LinkedHashSet"));
+	        System.out.println("TreeSet: " + sets.get("TreeSet"));
+	        
+	        scanner.close();
+	    }
+
+	    public static Map<String, Set<Integer>> createSets(List<Integer> list) {
+	        Set<Integer> hashSet = new HashSet<>(list); // Unique elements, no specific order
+	        Set<Integer> linkedHashSet = new LinkedHashSet<>(list); // Unique elements, insertion order
+	        Set<Integer> treeSet = new TreeSet<>(list); // Unique elements, sorted order
+
+	        Map<String, Set<Integer>> result = new HashMap<>();
+	        result.put("HashSet", hashSet);
+	        result.put("LinkedHashSet", linkedHashSet);
+	        result.put("TreeSet", treeSet);
+	        return result;
 	}
 
-	private static void compareSets() {
-		// Initialize sets
-		Set<Integer> hashSet = new HashSet<>();
-		Set<Integer> linkedHashSet = new LinkedHashSet<>();
-		Set<Integer> treeSet = new TreeSet<>();
-
-		// Measure performance for each set
-		measureSetPerformance("HashSet", hashSet);
-		measureSetPerformance("LinkedHashSet", linkedHashSet);
-		measureSetPerformance("TreeSet", treeSet);
-	}
-
-	private static void measureSetPerformance(String setType, Set<Integer> set) {
-		long startTime, endTime;
-
-		// Add operation
-		startTime = System.nanoTime();
-		for (int i = 0; i < DATA_SIZE; i++) {
-			set.add(i);
-		}
-		endTime = System.nanoTime();
-		System.out.println(setType + " - Add: " + (endTime - startTime) + " ns");
-
-		// Contains operation
-		startTime = System.nanoTime();
-		for (int i = 0; i < DATA_SIZE; i++) {
-			set.contains(i);
-		}
-		endTime = System.nanoTime();
-		System.out.println(setType + " - Contains: " + (endTime - startTime) + " ns");
-
-		// Remove operation
-		startTime = System.nanoTime();
-		for (int i = 0; i < DATA_SIZE; i++) {
-			set.remove(i);
-		}
-		endTime = System.nanoTime();
-		System.out.println(setType + " - Remove: " + (endTime - startTime) + " ns");
-	}
 }
